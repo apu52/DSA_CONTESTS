29546ee0de210b703998b4db0860f1df060f6b73
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-12-01 09:04:22 +0530

    Create Smallest Number With All Set Bits.py

---
+++ Leetcode_Weekly_Contest/Contest_426/Smallest Number With All Set Bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        
+        x = n
+        
+        
+        while (x & (x + 1)) != 0:
+            x += 1
+        
+        return x
+
+        ©leetcode
