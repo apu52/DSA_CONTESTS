d76dc707636622eda9fe7bbdf93d0c54b61bad24
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-12-01 08:04:49 +0530

    Create Q1. Smallest Number With All Set Bits.py

---
+++ Q1. Smallest Number With All Set Bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        
+        x = n
+        
+        
+        while (x & (x + 1)) != 0:
+            x += 1
+        
+        return x
