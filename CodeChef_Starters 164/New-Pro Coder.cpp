088e2e0fc13a44c34fad7103af1d33a3616e2b62
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-12-11 21:56:27 +0530

    Create New-Pro Coder.cpp

---
+++ CodeChef_Starters 164/New-Pro Coder.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    
+    if (M >= (N + 1) / 2) {
+        cout << "NEWBIE" << endl;
+    } else {
+        cout << "PRO" << endl;
+    }
+
+    return 0;
+}
