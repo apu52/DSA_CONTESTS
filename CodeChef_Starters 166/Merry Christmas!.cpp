e91bb472ece0eb1a3ef15a750eac085304649f5f
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-12-25 21:28:56 +0530

    Create Merry Christmas!.cpp

---
+++ CodeChef_Starters 166/Merry Christmas!.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int X;
+    cin >> X;
+
+    
+    vector<int> activities = {1, 2, 4};
+
+    
+    sort(activities.begin(), activities.end());
+
+    int count = 0; 
+    for (int duration : activities) {
+        if (X >= duration) {
+            X -= duration; 
+            count++;       
+        } else {
+            break; 
+        }
+    }
+
+    
+    cout << count << endl;
+
+    return 0;
+}
